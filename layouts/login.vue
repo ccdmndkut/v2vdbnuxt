47c3cc042e2ee56ea3154cce01621f80bd9038eb
ccdmndkut <37282688+ccdmndkut@users.noreply.github.com>
2018-12-17 23:09:34 -0500

    login

---
+++ layouts/login.vue
@@ -0,0 +1,107 @@
+<template>
+  <div id="terminal">
+    <div class="landing-view">
+      <nuxt/>
+    </div>
+  </div>
+</template>
+<style>
+@import url('https://fonts.googleapis.com/css?family=Press+Start+2P');
+* {
+  font-family: 'Press Start 2P', cursive;
+}
+
+html,
+body {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: black;
+  flex-direction: column;
+  color: #54fe55;
+}
+
+*::selection {
+  background: #1a4f1a;
+}
+
+.welcome .line {
+  /* overflow: hidden; */
+  width: 0px;
+  text-shadow: 0px 0px 10px #54fe55;
+  height: 2rem;
+  text-align: left;
+}
+.welcome .line1 {
+  animation: type 0.5s 1s steps(20, end) forwards;
+}
+.welcome .line2 {
+  animation: type 0.5s 1.5s steps(20, end) forwards;
+}
+.welcome .line3 {
+  animation: type 0.5s 2s steps(20, end) forwards;
+}
+
+@keyframes type {
+  to {
+    width: 280px;
+  }
+}
+form {
+  display: flex;
+  flex-direction: column;
+  max-width: 300px;
+}
+form label {
+  margin-bottom: 1rem;
+  display: flex;
+  flex-direction: column;
+  text-shadow: 0px 0px 10px #54fe55;
+}
+form input {
+  background: black;
+  border: 1px solid #54fe55;
+  margin-top: 0.5rem;
+  padding: 0.5rem;
+  color: #54fe55;
+  text-shadow: 0px 0px 10px #54fe55;
+  box-shadow: 0px 0px 5px #54fe55;
+}
+form input:focus {
+  outline: none;
+}
+form button {
+  padding: 0.5rem;
+  background: #1a4f1a;
+  color: #54fe55;
+  border: 0;
+  text-shadow: 0px 0px 10px #54fe55;
+  box-shadow: 0px 0px 10px #1a4f1a;
+  cursor: pointer;
+}
+form button:focus {
+  outline: none;
+}
+
+@keyframes blink {
+  0% {
+    opacity: 0;
+  }
+  49% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+.blink {
+  animation-name: blink;
+  animation-duration: 1s;
+  animation-iteration-count: infinite;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,60 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <div class="text-xs-center"></div>
-      <v-card>
-        <v-text-field v-model="email" label="username"></v-text-field>
-        <v-text-field v-model="password" label="password"></v-text-field>
-        <template v-if="user">
-          <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
-          <h2>{{user}}</h2>
-        </template>
-        <template v-if="error">
-          <div>{{error}}</div>
-        </template>
+  <div>
+    <div v-if="!user">
+      <form action>
         <div>
-          <div>
-            <v-btn @click="login">login</v-btn>
-            <v-btn @click="logout">logout</v-btn>
-          </div>
+          <template v-if="error">
+            <div class="welcome b">
+              <div class="line line1">{{error}}</div>
+            </div>
+          </template>
+          <template v-else>
+            <div class="welcome">
+              <div class="line line1">Login</div>
+              <div class="line line3">
+                <span v-if="!email" class="blink">_</span>
+              </div>
+            </div>
+          </template>
         </div>
-      </v-card>
-    </v-flex>
-  </v-layout>
+
+        <label for>
+          Email:
+          <input v-model="email" type="text">
+        </label>
+        <label for>
+          Password:
+          <input v-model="password" type="password">
+        </label>
+        <button @click.prevent="login">[ login ]</button>
+      </form>
+    </div>
+    <div v-else>Welcome</div>
+  </div>
+
+  <!-- <div>
+    <v-text-field v-model="email" label="username"></v-text-field>
+    <v-text-field v-model="password" label="password"></v-text-field>
+    <template v-if="user">
+      <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
+      <h2>{{user}}</h2>
+    </template>
+    <template v-if="error">
+      <div>{{error}}</div>
+    </template>
+    <div>
+      <div>
+        <v-btn @click="login">login</v-btn>
+        <v-btn @click="logout">logout</v-btn>
+      </div>
+    </div>
+  </div>-->
 </template>
 
 <script>
 import firebase from 'firebase'
 export default {
+  layout: 'login',
   data() {
     return {
       email: '',
@@ -65,3 +94,14 @@ export default {
   }
 }
 </script>
+<style>
+.b {
+  margin-bottom: 35px;
+  line-height: 20px;
+}
+.blink {
+  position: relative;
+  top: 60px;
+  left: 7px;
+}
+</style>
